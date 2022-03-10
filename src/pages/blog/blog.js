cc6e797ca4e3daf5d5faa71b0d1c21e94e00cd49
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-03-09 18:34:45 -0700

    gatsbyimage

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -1,32 +1,25 @@
 import * as React from 'react'
-import Layout from '../components/layout'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
-
-  
-
   return (
     <Layout pageTitle="My Blog Posts">
-      <div className='py-5'>
       {
-        data.allMdx.nodes.map((node) => (
-          <article className='py-5' key={node.id}>
-            <h2 className='text-xl font-bold'>{node.frontmatter.title}</h2>
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <h2 className='text-3xl'>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
           </article>
         ))
       }
-      </div>
     </Layout>
   )
 }
-
 export const query = graphql`
   query {
     allMdx(sort: {fields: frontmatter___date, order: DESC}) {
@@ -36,10 +29,9 @@ export const query = graphql`
           title
         }
         id
-        body
+        slug
       }
     }
   }
 `
-
 export default BlogPage
